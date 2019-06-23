a70e05aac515abbb2f73c72eda0f20bd21dcee1e
Joscelyn <JoscelynRose@gmail.com>
2019-06-23 11:04:32 -0400

    added some styling for keys after press

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,8 +2,8 @@
 window.addEventListener('keydown', function(event) {
     const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
     const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
-    console.log(key);
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
 })
