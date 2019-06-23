9e64de801db76afa79488bd73cf50cf8d6ab3ed5
Joscelyn <JoscelynRose@gmail.com>
2019-06-23 11:35:43 -0400

    WIP: getting style to transition

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,25 @@
 
-window.addEventListener('keydown', function(event) {
+function removeTransition(event) {
+    if (event.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+function playSound(event) {
     const audio = document.querySelector(`audio[data-key='${event.keyCode}']`);
     const key = document.querySelector(`.key[data-key='${event.keyCode}']`);
     if (!audio) return;
+
+    key.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
-    key.classList.add('playing');
-})
+}
+
+
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
